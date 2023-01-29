cb5e0943fdebbb7919a9e80c8791dbbbc1f43c62
Dinesh Yalavarthi <spicemovespicy@gmail.com>
2023-01-28 22:58:46 -0600

    added more ques and ans

--- hackerrank_level2.sql
+++ hackerrank_level2.sql
@@ -16,8 +16,6 @@ select ((select count(city) from station)-(select count(distinct city) from stat
 ---If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
 
 
-select distinct CITY,len(CITY) as length from STATION 
-
 with ct1 as (select city, len(city) as Leng, ROW_NUMBER() OVER (ORDER BY len(city),city ASC) as RowNum from station)
 (select CITY,len(CITY) from ct1 where RowNum = (select count(id) from station) or RowNum=1)
 
@@ -54,3 +52,43 @@ select distinct city from STATION where left(city,1) not in ('a', 'e', 'i', 'o',
 ---Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 
 select distinct city from STATION where left(city,1) not in ('a', 'e', 'i', 'o', 'u') and right(city,1) not in ('a','e','i','o','u')
+
+
+---Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
+---If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+create table student(
+id int,
+name varchar(20),
+markes int
+)
+
+select * from student
+
+insert into student values (1, 'Ashley', 81), (2, 'Samantha', 75), (3, 'Julia', 76), (4, 'Belvet', 84)
+
+select name from student where markes > 75 order by right(name,3) Asc, id Asc
+
+---Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+---where employee_id is an employee's ID number, name is their name, 
+---months is the total number of months they've been working for the company, and salary is their monthly salary
+
+create table employee(
+employee_id int,
+name nvarchar(30),
+months int,
+salary int
+)
+
+select * from employee
+
+insert into employee values (12228,'Rose',15,1968),(33645,'Angela',1,3443),(45692,'Frank',17,1608),(56118,'Patrick',7,1345),(59725,'Lisa',11,2330),(74197,'Kimberly',16,4372),
+(78454,'Bonnie',8,1771),(83565,'Michael',6,2017),(98607,'Todd',5,3396),(99989,'Joe',9,3573)
+
+select name from employee order by name Asc
+
+---Write a query that prints a list of employee names (i.e.: the name attribute) 
+---for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months.
+---Sort your result by ascending employee_id
+
+select name from employee where salary > 2000 and months < 10 order by employee_id Asc
